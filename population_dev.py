c7f2d3f72209355c4a153e53309ad3cd5e4a64e9
Martin Erhardt <martin.erhardt98@googlemail.com>
2014-09-29 22:06:10 +0200

    *calc

--- population_dev.py
+++ population_dev.py
@@ -15,18 +15,16 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
-population=[12300000,39100000,15500000,16300000]# read only until writeback
-population_eldered=[0,0,0,0]			# write only until writeback
+population=[12300000,39100000,15500000,16300000]
 population_change=(0.066,0.029,0.066)		# these are some constants
 def calc_year():
 	global population # I know, what I'm doing python
-	population_eldered[0]	= population[0]+round(population[1]*0.02) # some get born
+	population[0]	= population[0]+round(population[1]*0.02) # some get born
 	for i in range(0,len(population)-1,1):	# ,someget older
 		pop_change_abs		= round(population[i]*population_change[i]) # (in absolute numbers)
-		population_eldered[i+1]	= population[i+1] + pop_change_abs
-		population_eldered[i]	= population[i]	  - pop_change_abs
-	population_eldered[3]	= round(population[3]*0.972) # and the others die
-	population=population_eldered[:]# writeback
+		population[i+1]	= population[i+1] + pop_change_abs
+		population[i]	= population[i]	  - pop_change_abs
+	population[3]	= round(population[3]*0.972) # and the others die
 def main(argv):
 	print("year\t all\t\t young\t\t adult\t\t mature\t\t old")
 	print_pop(0)
@@ -37,7 +35,6 @@ def main(argv):
 	except (IndexError, ValueError) as e:
 		print(e)
 		print("Please give an integral index as command line argument!")
-	print("not very realistic ;)")
 def print_pop(i):
 	print(i,'\t',sum(population),'\t',population[0],'\t',population[1],'\t',population[2],'\t',population[3])
 if  __name__ =='__main__':main(sys.argv)
